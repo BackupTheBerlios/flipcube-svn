c9a38ae136046c42df193584d504e5b58660333d
larsh <larsh@f26a5ecd-55e4-0310-85c0-870042c23ad7>
2004-09-27 18:48:54 +0000

    created README.txt, modified a couple of files for public consumption and updated some obsolete information.

--- trunk/MultiTexturedBox.java
+++ trunk/MultiTexturedBox.java
@@ -4,6 +4,12 @@
  * which allows the use of different textures on each of its six faces.
  */
 
+import com.sun.j3d.utils.geometry.*;
+
+// Q: will different Facelets on the same Face have the same Appearance,
+// given that they share a Texture but use it at different offsets?
+
 public class MultiTexturedBox extends Box {
-	
+
+	// use e.g. getShape(Box.LEFT).setAppearance(...)
 }
